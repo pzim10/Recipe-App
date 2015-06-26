afc2e56b6147e2c44b5e5004ef27f6bd94bad1ea
Peter Zimmerman <pzim10@gmail.com>
2015-06-26 17:06:03 -0600

    Instantiate rootNavigationController

---
+++ Recipe App/RAViewController.h
@@ -0,0 +1,13 @@
+//
+//  RAViewController.h
+//  Recipe App
+//
+//  Created by Peter Zimmerman on 6/26/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RAViewController : UIViewController
+
+@end
